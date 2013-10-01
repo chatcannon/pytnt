435c19a7b66c03ff484336a2a7b457563cfb8d11
Chris Kerr <cjk34@cam.ac.uk>
2013-10-02 00:00:35 +0100

    Added tests for more test files (all failing)

--- test_pytnt.py
+++ test_pytnt.py
@@ -51,6 +51,51 @@ class TestFourierTransform(unittest.TestCase):
         # Define the absolute tolerance relative to the noise level
         tolerance = np.median(abs(freq_domain.DATA)) / 20
         assert_allclose(my_ft, freq_domain.DATA, atol=tolerance, rtol=1e-5)
+    
+    def test_ref2(self):
+        time_domain = TNTfile("testdata/LiCl_ref2.tnt")
+        freq_domain = TNTfile("testdata/LiCl_ref2-ftp.tnt")
+        
+        lb = freq_domain.linebrd[0]
+        ph0 = freq_domain.cumm_0_phase[0]
+        ph1 = freq_domain.cumm_1_phase[0]
+        
+        my_ft = time_domain.LBfft(lb * np.pi, 1, phase=np.deg2rad(ph0),
+                                  ph1 = np.deg2rad(ph1)) / 128
+        
+        # Define the absolute tolerance relative to the noise level
+        tolerance = np.median(abs(freq_domain.DATA)) / 20
+        assert_allclose(my_ft, freq_domain.DATA, atol=tolerance, rtol=1e-5)
+    
+    def test_ref3(self):
+        time_domain = TNTfile("testdata/LiCl_ref3.tnt")
+        freq_domain = TNTfile("testdata/LiCl_ref3-ftp.tnt")
+        
+        lb = freq_domain.linebrd[0]
+        ph0 = freq_domain.cumm_0_phase[0]
+        ph1 = freq_domain.cumm_1_phase[0]
+        
+        my_ft = time_domain.LBfft(lb * np.pi, 1, phase=np.deg2rad(ph0),
+                                  ph1 = np.deg2rad(ph1)) / 128
+        
+        # Define the absolute tolerance relative to the noise level
+        tolerance = np.median(abs(freq_domain.DATA)) / 20
+        assert_allclose(my_ft, freq_domain.DATA, atol=tolerance, rtol=1e-5)
+    
+    def test_ref4(self):
+        time_domain = TNTfile("testdata/LiCl_ref4.tnt")
+        freq_domain = TNTfile("testdata/LiCl_ref4-ftp.tnt")
+        
+        lb = freq_domain.linebrd[0]
+        ph0 = freq_domain.cumm_0_phase[0]
+        ph1 = freq_domain.cumm_1_phase[0]
+        
+        my_ft = time_domain.LBfft(lb * np.pi, 1, phase=np.deg2rad(ph0),
+                                  ph1 = np.deg2rad(ph1)) / 128
+        
+        # Define the absolute tolerance relative to the noise level
+        tolerance = np.median(abs(freq_domain.DATA)) / 20
+        assert_allclose(my_ft, freq_domain.DATA, atol=tolerance, rtol=1e-5)
 
 
 if __name__ == '__main__':
