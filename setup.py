a1bc01f5e4b2c0655a0f1a41a315c1676f6dfbde
Chris Kerr <chris.kerr@mykolab.ch>
2020-11-01 09:10:04 +0200

    Change licence metadata in setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup
 
 setup(
     name='pytnt',
-    version='0.0.1a1',
+    version='0.0.2',
     description="Open and process data files from TecMag's .tnt file format",
     url='https://github.com/chatcannon/pytnt',
     author='Chris Kerr',
-    license='BSD',
+    license='GPLv3+',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English'],
     packages=['pytnt'],
     scripts=['scripts/find_TNMR_backup_files.py'],  # TODO make this use entry_points
