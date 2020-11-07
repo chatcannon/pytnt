a1724736771fa0b205479c8764d0f68f2a9b9e36
Chris Kerr <chris.kerr@mykolab.ch>
2020-11-07 07:50:39 +0200

    Add more metadata to setup.py

--- setup.py
+++ setup.py
@@ -20,18 +20,25 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/chatcannon/pytnt',
     author='Chris Kerr',
+    author_email='chris.kerr@mykolab.ch',
     license='GPLv3+',
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: English'],
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering :: Chemistry',
+    ],
     packages=['pytnt'],
     entry_points={
         'console_scripts': [
             'find_TNMR_backup_files = pytnt.find_TNMR_backup_files:main',
         ],
     },
-    install_requires=['numpy']
+    python_requires='>=2.7',
+    install_requires=['numpy'],
+    tests_require=['pytest'],
 )
